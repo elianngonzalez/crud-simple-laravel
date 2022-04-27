a4a49cbeb8f561d77299164a86cec9fca7df8ee5
elian gonzalez <elianngonzalez7@gmail.com>
2022-04-27 00:56:56 -0300

    app

---
+++ app/Http/Controllers/NivelesController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nivel;
+use Illuminate\Http\Request;
+//session
+use Session;
+
+class NivelesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //paginate
+        $niveles = Nivel::all();
+        return view("niveles.inicio", ["niveles"=>$niveles]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("niveles.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $nivel = new Nivel($request->input());
+        $nivel->save();
+        return redirect()->route("niveles.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nivel  $nivel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nivel $nivel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nivel  $nivel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nivel $nivel)
+    {
+        return view("niveles.edit", ["nivel"=>$nivel]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nivel  $nivel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nivel $nivel)
+    {
+        $nivel->fill($request->input())->saveOrFail();
+        return redirect()->route("niveles.index")->with(["mensaje" => "Nivel actualizado"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nivel  $nivel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nivel $nivel)
+    {
+        $nivel->delete();
+        return redirect()->route("niveles.index");
+    }
+}
